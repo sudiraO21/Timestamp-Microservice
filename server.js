9bebba520946ce9bffa49fde8a1c0569c531f66a
sudiraO21 <68538727+sudiraO21@users.noreply.github.com>
2021-06-14 08:34:41 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+// timestamp API
+app.get('/api/:dateString?', (req, res) => {
+  const dateString = req.params.dateString;
+  let date;
+  // Empty Checking
+  if (!dateString) {
+    date = new Date();
+  } else {
+    // Integer Checking
+    if (!isNaN(dateString)) {
+      date = new Date(parseInt(dateString));
+    } else {
+      date = new Date(dateString);
+    }
+  }
+  // Invalid Checking
+  if (date.toString() === 'Invalid Date') {
+    res.json({ error: date.toString() });
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
